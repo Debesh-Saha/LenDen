9450111b1f2c0f6a58619c2f549879f09bcbf9ac
Debesh-Saha <debeshsaha2@gmail.com>
2026-06-07 19:04:43 +0530

    Updated code for Users.tsx

--- frontend/src/components/Users.tsx
+++ frontend/src/components/Users.tsx
@@ -1,34 +1,57 @@
 import { useState } from "react";
 import { UserCard } from "./UserCard";
+import { SendMoneyModal } from "./SendMoneyModal";
+
+type UserType = {
+    _id: number;
+    firstName: string;
+    lastName: string;
+};
 
 export const Users = () => {
-    const [users] = useState([
+    const [users] = useState<UserType[]>([
         {
             firstName: "Debesh",
             lastName: "Saha",
             _id: 1,
-        }, {
+        },
+        {
             firstName: "Monkey D",
             lastName: "Luffy",
             _id: 2,
-        }
+        },
     ]);
 
+    const [selectedUser, setSelectedUser] = useState<UserType | null>(null);
     return (
-        <div className="mt-1 pb-2 mx-2 bg-accent/70 rounded-lg ">
-            <div className="text-xl font-bold px-5 h-12 flex items-center border-b-3 border-text-secondary/40 text-text-secondary select-none">
-                Users
+        <>
+            <div className="mt-1 pb-2 mx-2 bg-accent/70 rounded-lg">
+
+                <div className="text-xl font-bold px-5 h-12 flex items-center border-b-3 border-text-secondary/40 text-text-secondary select-none">
+                    Users
+                </div>
+
+                <div className="mt-2 px-2">
+                    <input type="text" placeholder="Search users..." className=" w-full rounded-lg bg-surface-alt/65 px-4 py-3 text-text-secondary placeholder:text-text-secondary outline-none "/>
+                </div>
+
+                <div className="mt-4 space-y-1">
+                    {users.map((user) => (
+                        <UserCard
+                            key={user._id}
+                            user={user}
+                            onSend={setSelectedUser}
+                        />
+                    ))}
+                </div>
             </div>
 
-            <div className="mt-2 px-2">
-                <input type="text" placeholder="Search users..." className="w-full rounded-lg bg-surface-alt/65 px-4 py-3 text-text-secondary font-normal placeholder:text-text-secondary outline-none transition"/>
-            </div>
-
-            <div className="mt-4 space-y-1">
-                {users.map((user) => (
-                    <UserCard key={user._id} user={user} />
-                ))}
-            </div>
-        </div>
+            {selectedUser && (
+                <SendMoneyModal
+                    user={selectedUser}
+                    onClose={() => setSelectedUser(null)}
+                />
+            )}
+        </>
     );
 };
