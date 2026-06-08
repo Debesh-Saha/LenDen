250885e0ba92782c64c052c1095621adda02e8f9
Debesh-Saha <debeshsaha2@gmail.com>
2026-06-08 20:36:53 +0530

    Added signup route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard } from './pages/Dashboard';
+import { Signup } from './pages/Signup';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/signup" element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
     </>
