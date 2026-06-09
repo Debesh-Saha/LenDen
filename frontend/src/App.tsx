fe8a7822a89d1182ee79e806cdc4645ece5081f3
Debesh-Saha <debeshsaha2@gmail.com>
2026-06-10 00:03:04 +0530

    Added signin route to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Dashboard } from './pages/Dashboard';
 import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/signup" element={<Signup/>}/>
+          <Route path="/signin" element={<Signin/>}/>
         </Routes>
       </BrowserRouter>
     </>
